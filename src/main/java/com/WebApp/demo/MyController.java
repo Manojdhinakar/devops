9d3a59646f81e6f81e9b787532eef96035b243fb
Manojdhinakar <manojdhinakar01@gmail.com>
2024-05-08 13:01:59 +0530

    My new Commit

---
+++ src/main/java/com/WebApp/demo/MyController.java
@@ -0,0 +1,13 @@
+package com.WebApp.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/myg")
+	public String mys() {
+		return "Okdone";
+	}
+}
